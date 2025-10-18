7da12dc5eb12d99c5659801ad260d288601c0e78
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-10-18 15:50:26 +0900

    feat: 실습 To Do List 만들기

---
+++ week02/lab01/index.js
@@ -0,0 +1,11 @@
+const input = document.querySelector(".input");
+const button = document.querySelector(".button");
+const list = document.querySelector(".list");
+
+button.addEventListener("click", () => {
+  const li = document.createElement("li");
+  li.textContent = input.value;
+
+  input.value = "";
+  list.appendChild(li);
+});

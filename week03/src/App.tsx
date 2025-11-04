597e82d4a760c43ade0f82a407cc73054e4ad558
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-11-04 20:46:51 +0900

    init: 초기 setting

---
+++ week03/src/App.tsx
@@ -0,0 +1,12 @@
+import "@styles/global.css";
+import Home from "@pages/Home";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ week03/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <div className="text-primary-900 heading-sb-20">Home</div>
+      <Button>Click me</Button>
+    </>
+  );
+}
